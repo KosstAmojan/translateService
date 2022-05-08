9248aa55a4d0e33985c56f1ba260dc281183efd2
KosstAmojan <norwooma@oregonstate.edu>
2022-05-07 23:46:17 -0400

    Removing inapplicable trademarking info.

--- main.py
+++ main.py
@@ -1,31 +1,3 @@
-# Copyright 2021 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# must install virtual python environment and appropriate libraries:
-# https://pypi.org/project/google-cloud-translate/ (will need to run pip
-# commands as myenv/pip/bin)
-
-# set environment variables (may want to change this in your .profile so you
-# don't have to set the environment variables each time you run the script)
-# for GOOGLE_APPLICATION_CREDENTIALS
-
-# if you have an M1 mac, you need to set these environment variables:
-# GRPC_PYTHON_BUILD_SYSTEM_OPENSSL=1
-# GRPC_PYTHON_BUILD_SYSTEM_ZLIB=1
-# run the following: pip install firebase-admin
-#
-
 from flask import Flask, request
 import json
 from googletrans import Translator
